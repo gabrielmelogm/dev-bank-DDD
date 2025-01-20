44ae553d1dd7c6ef3b2899306636f820bce9ded8
Gabriel Melo <melogoncalvesbiel@gmail.com>
2025-01-20 16:47:45 -0300

    test: test bank account and transfer service

--- src/@core/domain/bank-account.service.spec.ts
+++ src/@core/domain/bank-account.service.spec.ts
@@ -2,12 +2,15 @@ import { DataSource, Repository } from "typeorm"
 import { BankAccountSchema } from "../infra/db/bank-account.schema"
 import { BankAccountTypeOrmRepository } from "../infra/db/bank-account-typeorm.repository"
 import { BankAccountService } from "./bank-account.service"
+import { BankAccount } from "./bank-account"
+import { TransferService } from "./transfer.service"
 
 describe('BankAccountTypeOrmRepository Test', () => {
   let dataSource: DataSource
   let ormRepo: Repository<BankAccountSchema>
   let repository: BankAccountTypeOrmRepository
   let bankAccountService: BankAccountService
+  let transferService: TransferService
 
   beforeEach(async () => {
     dataSource = new DataSource({
@@ -21,6 +24,7 @@ describe('BankAccountTypeOrmRepository Test', () => {
     ormRepo = dataSource.getRepository(BankAccountSchema)
     repository = new BankAccountTypeOrmRepository(ormRepo)
     bankAccountService = new BankAccountService(repository)
+    transferService = new TransferService()
   })
 
   it('should create a new bank account', async () => {
@@ -29,5 +33,47 @@ describe('BankAccountTypeOrmRepository Test', () => {
 
     expect(model.balance).toBe(0)
     expect(model.account_number).toBe('9999-99')
+  })
+
+  it('should be find one bank account by id', async () => {
+    await bankAccountService.create('9999-99')
+    const model = await ormRepo.findOneBy({ account_number: '9999-99' })
+    const bankAccount = await bankAccountService.findOne(model.id)
+
+    expect(bankAccount.balance).toBe(0)
+    expect(bankAccount.account_number).toBe('9999-99')
+  })
+
+  it('should be find all accounts', async () => {
+    const idsToCreate: string[] = ['1111-11', '2222-22', '3333-33']
+
+    for (const id of idsToCreate) {
+      await bankAccountService.create(id)
+    }
+
+    const bankAccounts = await bankAccountService.findAll()
+
+    expect(bankAccounts).toBeInstanceOf(Array<BankAccount>)
+  })
+
+  it('should be transfer an amount from an account to other account', async () => {
+    const idSource = '1111-11'
+    const idDest = '2222-22'
+
+    const amount = 50
+
+    await bankAccountService.create(idSource)
+    await bankAccountService.create(idDest)
+
+    const bankAccountSrc = await repository.findByAccountNumber(idSource)
+    const bankAccountDest = await repository.findByAccountNumber(idDest)
+
+    expect(bankAccountSrc.balance).toBe(0)
+    expect(bankAccountDest.balance).toBe(0)
+
+    transferService.transfer(bankAccountSrc, bankAccountDest, amount)
+
+    expect(bankAccountSrc.balance).toBe(-50)
+    expect(bankAccountDest.balance).toBe(50)
   })
 })
